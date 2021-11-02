3a4120b10db8f5a1b551b75a900f5e6a7613c873
Flavio Andrade <foa12381@gmail.com>
2021-11-02 16:31:45 -0300

    create taskModel

---
+++ src/models/taskModel.js
@@ -0,0 +1,19 @@
+const connection = require('./connectionMongo')
+
+const addTasks = async () => {}
+
+const getTasksAll = async () => {}
+
+const getTasksById = async () => {}
+
+const editTask = async () => {}
+
+const destroyTask = async () => {}
+
+module.exports = {
+  addTasks,
+  getTasksAll,
+  getTasksById,
+  editTask,
+  destroyTask,
+}
